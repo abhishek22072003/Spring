c7fb24802235e977c6b2df814768e4cb188140de
abhishek22072003 <abhi2003patil@gmail.com>
2024-07-16 14:19:00 +0530

    Implementation of lifecycle Methods completed

---
+++ springcore/src/main/java/com/springcore/lifecycle/Samosa.java
@@ -0,0 +1,30 @@
+package com.springcore.lifecycle;
+
+public class Samosa {
+	private double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Samosa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Samosa [price=" + price + "]";
+	}
+	public void init() {
+		System.out.println("inside init");
+	}
+	public void destroy() {
+		System.out.println("inside destroy");
+	}
+	
+}
---
+++ springcore/src/main/java/com/springcore/lifecycle/Test.java
@@ -0,0 +1,22 @@
+package com.springcore.lifecycle;
+
+import javax.swing.AbstractAction;
+import javax.swing.text.AbstractDocument.Content;
+
+import org.springframework.context.*;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
+		context.registerShutdownHook();
+//		Samosa samosa = (Samosa)context.getBean("samosa");
+//		System.out.println(samosa);		
+//		System.out.println("++++++++++++++++++++++++++++++++++++++++");
+//		Pepsi pepsi = (Pepsi)context.getBean("pepsi");
+//		System.out.println(pepsi);
+		Example example = (Example)context.getBean("example");
+		System.out.println(example);
+	}
+}
